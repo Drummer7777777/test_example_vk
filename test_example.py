dabb472f9b64b4aec0625c9af8c79c4b47e95d38
Drummer7777777 <newlogan95@mail.ru>
2022-09-13 15:54:49 +0500

    done answer

--- test_example.py
+++ test_example.py
@@ -1,13 +1,73 @@
 import pytest
 
 
-# @pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6), ("6*9", 42)])
-# def test_eval(test_input, expected):
-#     assert eval(test_input) == expected
-
-@pytest.mark.parametrize("test_input,expected", [
-    ([1, 2, 3], 6),
-    ([5, 6, 7], 10),
+@pytest.mark.parametrize('test_input,expected', [
+    (['auto', 'test'], 8),
+    (['', 'test'], 4),
+    (['auto', ''], 4),
+    (['', ''], 0),
+    ([25, 25], 4),
+    ([25], 2),
+    ([], 0),
+    ([-2], 2)
 ])
-def test_sum(test_input, expected):
+def test_len_str(test_input, expected):
+    try:
+        assert sum(map(len, test_input)) == expected
+    except TypeError:
+        assert sum(map(len, map(str, test_input))) == expected
+
+
+@pytest.mark.parametrize('test_input,expected', [
+    ('auto', 'AUTO'),
+    ('Auto', 'AUTO'),
+    ('AUTO', 'AUTO')
+])
+def test_upper_str(test_input, expected):
+    assert test_input.upper() == expected
+
+
+@pytest.mark.parametrize('test_input,expected', [
+    ('auto', 'Auto'),
+    ('aUto', 'Auto'),
+    ('Auto', 'Auto')
+])
+def test_title_str(test_input, expected):
+    assert test_input.title() == expected
+
+
+@pytest.mark.parametrize('test_input,expected', [
+    ([1, 1], 2),
+    ([], 0),
+    ([1, -1], 0),
+    ([-1, -1], -2),
+    ([1], 1)
+])
+def test_sum_int(test_input, expected):
     assert sum(test_input) == expected
+
+
+@pytest.mark.parametrize('test_input,expected', [
+    ([1, 1], 0),
+    ([-1], -1),
+    ([-1, -1], -2)
+])
+def test_raz_int(test_input, expected):
+    try:
+        if test_input[1] < 0:
+            assert test_input[0] + test_input[1] == expected
+        else:
+            assert test_input[0] - test_input[1] == expected
+    except IndexError:
+        if len(test_input) == 1:
+            assert test_input[0] == expected
+
+
+@pytest.mark.parametrize('test_input,expected', [
+    ([2, 2], 4),
+    ([0, 0], 1),
+    ([2, -2], 0.25),
+    ([-2, 2], 4)
+])
+def test_degree_int(test_input, expected):
+    assert test_input[0] ** test_input[1] == expected
